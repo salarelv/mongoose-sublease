d957a3a52ef1de8e34e63cbd49f954d804d1a751
Taavi <taavi@root.ee>
2019-03-23 12:56:07 +0200

    Updated hapi plugin structure to newer hapijs 17

--- hapi.js
+++ hapi.js
@@ -3,46 +3,45 @@
 const sublease = require('./')
 var channels = [];
 var models = {};
-
-exports.register = function(server, _options, next) {
-  _options = Object.assign({
-    connectionKey: 'connection',
-    tenantKey: 'tenant',
-    modelKey: 'model',
-    getDbName: (req, connection) => connection.name,
-  }, _options);
-
-  channels.push(_options);
-
-  for (var i in _options.models) {
-    if (models[i]) {
-      console.log("Warning!", "Model " + i + " already inserted");
-    } else {
-      models[i] = channels.length - 1;
-    }
-  }
-
-  server.ext("onPreAuth", function(request, reply) {
-    function modelFunc(modelName, dbName) {
-      var channel = channels[models[modelName]];
-      var getTenant = sublease(channel.connection, channel.models);
-      if (!dbName)
-        dbName = channel.getDbName(request, channel.connection)
-      const tenant = getTenant(dbName)
-      return (tenant.model.bind(tenant))(modelName);
-    };
-
-    if(typeof request.app === "undefined")
-      request.app = {};
-
-    request.app[_options.modelKey] = modelFunc;
-    return reply.continue();
-  });
-  next();
-};
-
-exports.register.attributes = {
-  name: 'mongoose-sublease',
+const plugin = {
+  name: 'URLGenerator',
   version: '1.0.0',
-  multiple: true
+  register: async function (server, options) {
+    _options = Object.assign({
+      connectionKey: 'connection',
+      tenantKey: 'tenant',
+      modelKey: 'model',
+      getDbName: (req, connection) => connection.name,
+    }, _options);
+
+    channels.push(_options);
+
+    for (var i in _options.models) {
+      if (models[i]) {
+        console.log("Warning!", "Model " + i + " already inserted");
+      } else {
+        models[i] = channels.length - 1;
+      }
+    }
+
+    server.ext("onPreAuth", function(request, reply) {
+      function modelFunc(modelName, dbName) {
+        var channel = channels[models[modelName]];
+        var getTenant = sublease(channel.connection, channel.models);
+        if (!dbName)
+          dbName = channel.getDbName(request, channel.connection)
+        const tenant = getTenant(dbName)
+        return (tenant.model.bind(tenant))(modelName);
+      };
+
+      if(typeof request.app === "undefined")
+        request.app = {};
+
+      request.app[_options.modelKey] = modelFunc;
+      return reply.continue();
+    });
+    next();
+  }
 };
+
+exports.plugin = plugin;
